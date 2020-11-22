2e69dfc674829f11a1606fa359d5b3e8df5c87b9
Itamar80 <65674792+Itamar80@users.noreply.github.com>
2020-11-22 17:36:23 +0200

    Uploading backend

---
+++ index.js
@@ -0,0 +1,83 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const cors = require('cors');
+
+
+const path = require('path')
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors({ origin: true }));
+
+// show workers
+async function getWorkers(req, res) {
+    const snapshot = await admin.firestore().collection('workers').get();
+    const workers = []
+    snapshot.forEach((doc) => {
+        let id = doc.id;
+        let data = doc.data();
+
+        workers.push({ id, ...data })
+    });
+    res.status(200).send(JSON.stringify(workers));
+}
+
+app.get('/', async (req, res) => {
+    await getWorkers(req, res)
+})
+
+// get worker by id
+app.get('/:id', async (req, res) => {
+    const snapshot = await admin.firestore().collection('workers').doc(req.params.id).get();
+
+    const userId = snapshot.id;
+    const userData = snapshot.data();
+
+    res.status(200).send(JSON.stringify({ id: userId, ...userData }));
+})
+
+// add worker
+app.post('/', async (req, res) => {
+    const body = req.body;
+
+    await admin.firestore().collection('workers').add(body);
+    getWorkers(req, res)
+})
+
+//update worker
+app.put("/:id", async (req, res) => {
+    const body = req.body;
+    console.log('put request', body);
+    await admin.firestore().collection("workers").doc(req.params.id).update({ name: body.name, email: body.email });
+    getWorkers(req, res)
+})
+
+//delete worker
+app.delete("/:id", async (req, res) => {
+    await admin.firestore().collection("workers").doc(req.params.id).delete();
+    getWorkers(req, res)
+})
+
+exports.user = functions.https.onRequest(app);
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.resolve(__dirname, 'public')));
+// } else {
+//     const corsOptions = {
+//         origin: ['http://127.0.0.1:19006', 'http://localhost:19006/'],
+//         credentials: true
+//     };
+// }
+
+// const logger = require('./services/logger.service')
+const port = 3000;
+// const router = express.Router();
+// app.use('/api', router);
+app.listen(port, () => {
+    console.log('Server is ready at ' + port);
+});
